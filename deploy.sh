09894e627ef9ca065a88bfa9976b9e2f6492a87c
Prashant Sinha <prashant@ducic.ac.in>
2016-04-25 00:57:55 +0530

    ^ fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 git branch -f master
 git checkout master
 git reset --hard origin/develop
+npm run build
+cp -r public/* .
+echo '0xc0d3.pw' > CNAME
 git add -A .
 git commit -a -m 'site update'
 git push origin master --force
-git checkout master
+git checkout develop
 git rev-parse --abbrev-ref HEAD
